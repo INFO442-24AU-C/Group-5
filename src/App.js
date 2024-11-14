7d201d9411c3c49fcb4a7e1dd9da9f22d690ad06
nzhou07 <nzhou07@uw.edu>
2024-11-13 17:05:56 -0800

    most recent

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import { HomePage } from "./components/main.js";
 
 import { Route, Routes, Navigate, useNavigate, Outlet } from "react-router-dom";
 import { getDatabase, DataSnapshot, ref, push as firebasePush, onValue, set as firebaseSet } from 'firebase/database';
+import {ProfilePage} from "./components/ProfilePage.js";
+import { RSOSection } from "./components/RSOsection.js";
 
 
 
 function App() {
 
-    return (
-      <div>
-        <HomePage/>
-      </div>
+  return (
+    <div>
+        <HomePage />
+        <Routes>
+            <Route path="/rso-and-events" element={<RSOSection />} />
+            <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+    </div>
     );
   }
 
--- src/components/main.js
+++ src/components/main.js
@@ -1,35 +1,4 @@
-// import { Navbar } from './Navbar.js';
-// import { SearchBar } from './Searchbar.js';
-// import { Events } from './Event.js';
-// import { RSOSection } from './RSOsection.js';
 
-// export function HomePage() {
-//   const container = document.createElement('div');
-//   container.classList.add('main-content');
-
-//   const navbar = Navbar();
-//   const searchBar = SearchBar();
-//   const backgroundSection = document.createElement('div');
-//   backgroundSection.classList.add('background-image');
-
-//   const eventsSection = Events();
-//   const rsoSection = RSOSection();
-
-//   container.append(searchBar, backgroundSection, eventsSection, rsoSection);
-
-//   const app = document.getElementById('root');
-//   app.appendChild(navbar);
-//   app.appendChild(container);
-// }
-
-
-  const app = document.getElementById('app');
-  //app.appendChild(navbar);
-  //app.appendChild(container);
-}
-
-
-// components/HomePage.js
 import React from 'react';
 import { NavBar } from './Navbar_2.js';
 import { SearchBar } from './Searchbar.js';
