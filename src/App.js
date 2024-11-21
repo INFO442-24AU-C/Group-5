a6bf57b71287dbd1d83d5a4d78f01774408f16b8
nzhou07 <nzhou07@uw.edu>
2024-11-20 22:53:21 -0800

    link event cards to details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Route, Routes, Navigate, useNavigate, Outlet } from "react-router-dom";
 import { React, useEffect, useState } from "react";
 import { NavBar } from "./components/Navbar_2.js";
@@ -7,68 +6,89 @@ import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import { getDatabase, ref, push, set as firebaseSet } from "firebase/database";
 
 
+
+
 import { HomePage } from "./components/main.js";
 // import { getDatabase, DataSnapshot, ref, push as firebasePush, onValue, set as firebaseSet } from 'firebase/database';
 import CreateProfilePage from "./components/CreateProfilePage.js";
 import { RSOSection } from "./components/RSOsection.js";
 import CreateEventPage from "./components/CreateEventPage.js";
 import { SignInPage } from "./SignInPage.js";
+import EventDetails from "./components/EventDetails.js";
+
+
 
 
 function App() {
-  // const [currentUser, setCurrentUser] = useState(null);
-  // const navigateTo = useNavigate();
-
-  // // console.log(currentUser);
-  // const database = getDatabase();
-
-  // // const userDataRef = ref(database, "userData");
+ // const [currentUser, setCurrentUser] = useState(null);
+ // const navigateTo = useNavigate();
 
 
-  // useEffect(() => {
-  //   const auth = getAuth();
-
-  //   onAuthStateChanged(auth, (user) => {
-  //     if (user) {
-  //       console.log("signing in as", user.displayName);
-  //       setCurrentUser(user);
-
-  //       // console.log("is not null");
-  //       if (user.uid != null) {
-  //         // console.log("test is not null")
-  //         const userNameRef = ref(database, "userData/" + user.uid + "/name");
-  //         const userEmailRef = ref(database, "userData/" + user.uid + "/email");
-  //         firebaseSet(userNameRef, user.displayName);
-  //         firebaseSet(userEmailRef, user.email);
-          
-  //       }
-        
+ // // console.log(currentUser);
+ // const database = getDatabase();
 
 
-  //     }
-  //     else {
-  //       console.log("signed out");
-  //       setCurrentUser(null);
-  //       navigateTo('/signin');
-  //     }
-  //   })
+ // // const userDataRef = ref(database, "userData");
 
 
-  // })
 
-  return (
-    <div>
-        <Routes>
-            <Route index element={<HomePage />} />
-            <Route path="home" element={<HomePage />} />
-            <Route path="/sign-in" element={<SignInPage />} />
-            <Route path="/rso-and-events" element={<RSOSection />} />
-            <Route path="/profile" element={<CreateProfilePage />} />
-            <Route path="/create-events" element={<CreateEventPage />} />
-        </Routes>
-    </div>
-  );
+
+ // useEffect(() => {
+ //   const auth = getAuth();
+
+
+ //   onAuthStateChanged(auth, (user) => {
+ //     if (user) {
+ //       console.log("signing in as", user.displayName);
+ //       setCurrentUser(user);
+
+
+ //       // console.log("is not null");
+ //       if (user.uid != null) {
+ //         // console.log("test is not null")
+ //         const userNameRef = ref(database, "userData/" + user.uid + "/name");
+ //         const userEmailRef = ref(database, "userData/" + user.uid + "/email");
+ //         firebaseSet(userNameRef, user.displayName);
+ //         firebaseSet(userEmailRef, user.email);
+
+
+ //       }
+
+
+
+
+
+
+ //     }
+ //     else {
+ //       console.log("signed out");
+ //       setCurrentUser(null);
+ //       navigateTo('/signin');
+ //     }
+ //   })
+
+
+
+
+ // })
+
+
+ return (
+   <div>
+       <Routes>
+           <Route index element={<HomePage />} />
+           <Route path="home" element={<HomePage />} />
+           <Route path="/sign-in" element={<SignInPage />} />
+           <Route path="/rso-and-events" element={<RSOSection />} />
+           <Route path="/profile" element={<CreateProfilePage />} />
+           <Route path="/create-events" element={<CreateEventPage />} />
+           <Route path="/event/:id" element={<EventDetails />} />
+       </Routes>
+   </div>
+ );
 }
+
+
 
 
 export default App;
---
+++ src/components/EventDetails.js
@@ -0,0 +1,49 @@
+
+// components/EventDetails.js
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { database } from "./firebaseConfig";
+import { ref, get } from "firebase/database";
+
+
+const EventDetails = () => {
+ const { id } = useParams(); // Get the event ID from the URL
+ const [event, setEvent] = useState(null);
+
+
+ useEffect(() => {
+   const eventRef = ref(database, `events/${id}`);
+   console.log("Fetching event with ID:", id);  // Log ID to ensure it's correct
+
+
+   get(eventRef)
+     .then((snapshot) => {
+       if (snapshot.exists()) {
+         console.log("Event data:", snapshot.val()); // Log event data to see if it's being fetched correctly
+         setEvent(snapshot.val());
+       } else {
+         console.error("Event not found for ID:", id);
+       }
+     })
+     .catch((error) => console.error("Error fetching event details:", error));
+ }, [id]);
+
+
+ if (!event) {
+   return <p>Loading event details...</p>;
+ }
+
+
+ return (
+   <div>
+     <h1>{event.name}</h1>
+     <p>{event.details}</p>
+     <p>{event.date}</p>
+     <p>{event.admission ? `$${event.admission}` : "Free Admission"}</p>
+     {/* Other event details */}
+   </div>
+ );
+};
+
+
+export default EventDetails;
