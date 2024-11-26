611fc86ef7baffdffe0d4b67d8521020b6eeceeb
mattchung25 <mchung25@uw.edu>
2024-11-25 17:22:36 -0800

    fixed the css of the event info box

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -45,52 +45,58 @@ const EventDetails = () => {
         <img
           src='/img/event.jpg'
           alt="event image"
-          className="event-image"
+          className="event-section-image"
         />
       </header>
-      {/* <div className="event-details">
-          <h1 className = "event-title">{event.name}</h1>
-          <p>{event.details}</p>
-          <p>{event.admission ? `$${event.admission}` : "Free Admission"}</p>
-      </div> */}
-    <main className="event-details">
-      <h1 className = "event-title-details">{event.name}</h1>
-        <div className='link-section'>
-          <a href={event.link} target="_blank">
-          <img 
-            src='/img/Instagram.png'
-            alt='instagram'
-            className='insta-links'
-          />
-        </a>
-        <a href={event.link} target="_blank">
-          <img 
-            src='/img/Link.png'
-            alt='link'
-            className='web-links'
-          />
-        </a>
-        <div className="event-info">
-          <p>
-            <img
-            src='/img/Calendar.png'
-            alt='calendar'
-            className='calendar-image'
-            />
-            {event.startTime} | {event.date}
-          </p>
-          <p>{event.admission ? `$${event.admission}` : "Free Admission"}</p>
+    <main className="event-section-details">
+      <div className="event-information">
+        <div>
+          <h1 className = "event-title-details">{event.name}</h1>
+          <div className='link-section'>
+            <a href={event.link} target="_blank">
+              <img 
+                src='/img/Instagram.png'
+                alt='instagram'
+                className='insta-links'
+              />
+            </a>
+            <a href={event.link} target="_blank">
+              <img 
+                src='/img/Link.png'
+                alt='link'
+                className='web-links'
+              />
+            </a>
+          </div>
         </div>
+          <p className="event-text">{event.details}</p>
       </div>
-      <p>{event.details}</p>
-      <p>{event.admission ? `$${event.admission}` : "Free Admission"}</p>
+      <div className="event-info">
+        <p>
+          <img
+          src='/img/Calendar.png'
+          alt='calendar'
+          className='event-icon-images'
+          />
+          {event.startTime} | {event.date}
+        </p>
+        <p>
+          <img
+          src='/img/Location.png'
+          alt='Location'
+          className='event-icon-images'
+          />
+          {event.location ? `$${event.location}` : "Unknown"}</p>
+        <p>
+          <img
+          src='/img/Receipt.png'
+          alt='Receipt'
+          className='event-icon-images'
+          />
+          {event.admission ? `$${event.admission}` : "Free Admission"}</p>
+        <p className='save-event-button'>Save</p>
+      </div>
     </main>
-    
-    {/* <h1>{event.name}</h1>
-    <p>{event.details}</p>
-    <p>{event.date}</p>
-    <p>{event.admission ? `$${event.admission}` : "Free Admission"}</p>
-     Other event details */}
     </div>
    </div>
  );
